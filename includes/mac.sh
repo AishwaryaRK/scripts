2f84a01a0a4476164523c5efb7c9cd334e05541f
Chad Furman <chad@chadfurman.com>
2015-06-03 16:26:35 -0400

    first pass at drying up code, still need to pull re-usable components out of mac include and test that original functionality still works...

---
+++ includes/mac.sh
@@ -0,0 +1,38 @@
+if [ \( -z "$ENV" \) -o \( "$ENV" == "development" \) ]; then
+  ### copy over plist if we're in development mode
+  cp sh.motd.generator.plist ~/Library/LaunchAgents/sh.motd.generator.plist.tmp
+else
+  ### otherwise download it
+  curl -sL "${PLIST_URL}" -o ~/Library/LaunchAgents/sh.motd.generator.plist.tmp
+fi
+
+## replace set user in plist to current user
+cat ~/Library/LaunchAgents/sh.motd.generator.plist.tmp | sed -e "s/USER/$USER/" > ~/Library/LaunchAgents/sh.motd.generator.plist
+
+## remove temporary file
+rm ~/Library/LaunchAgents/sh.motd.generator.plist.tmp
+
+## schedule jobs
+## http://nathangrigg.net/2012/07/schedule-jobs-using-launchd/
+launchctl load -w ~/Library/LaunchAgents/sh.motd.generator.plist
+launchctl start sh.motd.generator
+
+## give the user a preview by opening terminal
+## http://stackoverflow.com/a/7177891
+#osascript -e 'tell application "System Events" to keystroke "t" using command down'
+
+echo "please add (or ensure something similar exists) the following to the top of your ~/.zshrc or ~/.bashrc file"
+echo ""
+echo ""
+echo "# ------ START OF SCRIPT ------"
+echo ""
+echo "# Display MotD"
+echo "if [[ -e \$HOME/.motd ]]; then cat \$HOME/.motd; fi"
+echo ""
+echo "# ------  END OF SCRIPT  ------"
+echo ""
+echo ""
+echo "successfully installed motd.sh"
+bash ~/.motd.sh
+echo "$TERMINAL"
+exit 1
--- install.sh
+++ install.sh
@@ -4,42 +4,76 @@ MOTD_URL="https://raw.githubusercontent.com/messageoftheday/scripts/master/.motd
 PLIST_URL="https://raw.githubusercontent.com/messageoftheday/scripts/master/sh.motd.generator.plist"
 TERMINAL="please open a new terminal window to view the motd"
 
-motd_install()
+# this defines commands used by the MOTD script
+motd_configure()
 {
-  echo "installing motd.sh"
-  # detect environment
-  # http://stackoverflow.com/a/3826462
-  # http://stackoverflow.com/a/17072017
-  if [ "$MOTD_ENV" = "development" ]; then
-    ## development
+  # install the locally bundled version of the MOTD script
+  motd_local_update()
+  {
     cp .motd.sh ~/.motd.sh
     ## replace http://motd.sh with http://localhost:3000
     sed -i bak -e "s/http:\/\/motd.sh/http:\/\/localhost:3000/" ~/.motd.sh
-  else
-    ## production
+  }
+
+  # fetch an MOTD script update from the server
+  motd_remote_update()
+  {
     ## download latest script
     curl -sL "${MOTD_URL}" -o ~/.motd.sh
-  fi
+  }
 
-  # prompt user for weather (y/n)
-  # if (y) then prompt for (auto - lookup by ip) or (enter your city, state/province)
+  #update the MOTD script according to environment
+  # http://stackoverflow.com/a/3826462
+  # http://stackoverflow.com/a/17072017
+  motd_update()
+  {
+    if [ "$MOTD_ENV" = "development" ]; then
+      ## development
+      motd_local_update
+    else
+      ## production
+      motd_remote_update
+    fi
+  }
+
+  # allow script to be executed
+  motd_set_executable()
+  {
+    chmod +x ~/.motd.sh
+  }
+
+  motd_configure_weather_address()
+  {
+    sed -i bak -e "s/WEATHER=\"\"/WEATHER=\"$1\"/" ~/.motd.sh
+  }
+
+  motd_configure_weather_degrees()
+  {
+    sed -i bak -e "s/DEGREES=\"\"/DEGREES=\"$1\"/" ~/.motd.sh
+  }
+
+  # ask if installer should configure weather
+  motd_prompt_weather()
+  {
+    # prompt user for weather (y/n)
+    # if (y) then prompt for (auto - lookup by ip) or (enter your city, state/province)
     while read -p "do you want to add the weather forecast to your motd? (y/n)" yn; do
       case $yn in
       [Yy]* )
         echo "please enter a location (e.g. address/city/location/zip)"
         read WEATHER
         WEATHER=`echo "$WEATHER"|sed 's/ /%20/g'`
-        sed -i bak -e "s/WEATHER=\"\"/WEATHER=\"$WEATHER\"/" ~/.motd.sh
+        motd_configure_weather_address $WEATHER
         # now we want to ask user if they want fahrenheit or celsius
         while read -p "do you want degrees in fahrenheit (y) or celsius (n)? (y/n)" yn; do
             case $yn in
                 [Yy]* )
                   echo "you selected fahrenheit!";
-                  sed -i bak -e "s/DEGREES=\"\"/DEGREES=\"f\"/" ~/.motd.sh
+                  motd_configure_weather_degrees f
                   break;;
                 [Nn]* )
                   echo "you selected celsius!";
-                  sed -i bak -e "s/DEGREES=\"\"/DEGREES=\"c\"/" ~/.motd.sh
+                  motd_configure_weather_degrees c
                   break;;
                 * ) echo "please answer fahrenheit (y) or celsius (n).";;
             esac
@@ -50,110 +84,95 @@ motd_install()
       * )
         echo "please answer yes (y) or no (n).";;
       esac
-  done
+    done
+  }
 
-  # prompt user for stocks (yn)
-  # if (y) then prompt for comma delimited stock symbols
-  while read -p "do you want to add stock quotes to your motd? (y/n)" yn; do
-      case $yn in
-          [Yy]* )
-            echo "you added stocks!";
-            read -p "please enter stocks on NASDAQ separated by a comma (.e.g TSLA,SCTY)" STOCKS
-            # strip whitespace from stocks
-            # http://unix.stackexchange.com/a/156581
-            STOCKS="$(echo "$STOCKS" | tr -d ' ')"
-            sed -i bak -e "s/STOCKS=\"\"/STOCKS=\"$STOCKS\"/" ~/.motd.sh
-            break;;
-          [Nn]* ) break;;
-          * ) echo "please answer yes (y) or no (n).";;
-      esac
-  done
+  motd_configure_stock()
+  {
+    sed -i bak -e "s/STOCKS=\"\"/STOCKS=\"$1\"/" ~/.motd.sh
+  }
 
-  # prompt user for quotes (y/n)
-  while read -p "do you want to add inspirational/motivational quotes to your motd? (y/n)" yn; do
-      case $yn in
-          [Yy]* )
-            echo "you added quotes!";
-            sed -i bak -e "s/QUOTES=\"\"/QUOTES=\"y\"/" ~/.motd.sh
-            break;;
-          [Nn]* )
-            sed -i bak -e "s/QUOTES=\"\"/QUOTES=\"n\"/" ~/.motd.sh
-            break;;
-          * ) echo "please answer yes (y) or no (n).";;
-      esac
-  done
+  # ask if installer should configure stocks
+  motd_prompt_stock()
+  {
+    # prompt user for stocks (yn)
+    # if (y) then prompt for comma delimited stock symbols
+    while read -p "do you want to add stock quotes to your motd? (y/n)" yn; do
+        case $yn in
+            [Yy]* )
+              echo "you added stocks!";
+              read -p "please enter stocks on NASDAQ separated by a comma (.e.g TSLA,SCTY)" STOCKS
+              # strip whitespace from stocks
+              # http://unix.stackexchange.com/a/156581
+              STOCKS="$(echo "$STOCKS" | tr -d ' ')"
+              motd_configure_stocks $STOCKS
+              break;;
+            [Nn]* ) break;;
+            * ) echo "please answer yes (y) or no (n).";;
+        esac
+    done
+  }
 
-  # allow script to be executed
-  chmod +x ~/.motd.sh
+  motd_configure_quotes()
+  {
+    sed -i bak -e "s/QUOTES=\"\"/QUOTES=\"$1\"/" ~/.motd.sh
+  }
 
+  # ask if installer should configure 
+  motd_prompt_quotes()
+  {
+    # prompt user for quotes (y/n)
+    while read -p "do you want to add inspirational/motivational quotes to your motd? (y/n)" yn; do
+        case $yn in
+            [Yy]* )
+              echo "you added quotes!";
+              motd_configure_quotes y
+              break;;
+            [Nn]* )
+              motd_configure_quotes n
+              break;;
+            * ) echo "please answer yes (y) or no (n).";;
+        esac
+    done
+  }
+
+  motd_configure_os()
+  {
   # detect os
-  echo "detecting operating system"
+    echo "detecting operating system"
 
-  if [ "$(uname)" == "Darwin" ]; then
-
-    ## mac os x
-    echo "detected mac os x"
-
-    if [ \( -z "$ENV" \) -o \( "$ENV" == "development" \) ]; then
-      ### copy over plist if we're in development mode
-      cp sh.motd.generator.plist ~/Library/LaunchAgents/sh.motd.generator.plist.tmp
+    if [ "$(uname)" == "Darwin" ]; then
+      ## mac os x
+      echo "detected mac os x"
+      source includes/mac.sh
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      ## linux
+      echo "detected linux"
+      source includes/linux.sh
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+      ## windows
+      ## TODO: add support
+      echo "detected windows"
+      echo "windows is not currently supported"
+      ## TODO: add github issue for this
+      echo ""
+      exit 1
     else
-      ### otherwise download it
-      curl -sL "${PLIST_URL}" -o ~/Library/LaunchAgents/sh.motd.generator.plist.tmp
+      echo "unknown operating system detecting"
+      exit 1
     fi
+  }
 
-    ## replace set user in plist to current user
-    cat ~/Library/LaunchAgents/sh.motd.generator.plist.tmp | sed -e "s/USER/$USER/" > ~/Library/LaunchAgents/sh.motd.generator.plist
+}
 
-    ## remove temporary file
-    rm ~/Library/LaunchAgents/sh.motd.generator.plist.tmp
-
-    ## schedule jobs
-    ## http://nathangrigg.net/2012/07/schedule-jobs-using-launchd/
-    launchctl load -w ~/Library/LaunchAgents/sh.motd.generator.plist
-    launchctl start sh.motd.generator
-
-    ## give the user a preview by opening terminal
-    ## http://stackoverflow.com/a/7177891
-    #osascript -e 'tell application "System Events" to keystroke "t" using command down'
-
-    echo "please add (or ensure something similar exists) the following to the top of your ~/.zshrc or ~/.bashrc file"
-    echo ""
-    echo ""
-    echo "# ------ START OF SCRIPT ------"
-    echo ""
-    echo "# Display MotD"
-    echo "if [[ -e \$HOME/.motd ]]; then cat \$HOME/.motd; fi"
-    echo ""
-    echo "# ------  END OF SCRIPT  ------"
-    echo ""
-    echo ""
-    echo "successfully installed motd.sh"
-    bash ~/.motd.sh
-    echo "$TERMINAL"
-    exit 1
-
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-
-    ## linux
-    echo "detected linux"
-
-    ## TODO: add support
-    echo "linux is not currently supported"
-    exit 1
-
-  elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    ## windows
-    ## TODO: add support
-    echo "detected windows"
-    echo "windows is not currently supported"
-    ## TODO: add github issue for this
-    echo ""
-    exit 1
-  else
-    echo "unknown operating system detecting"
-    exit 1
-  fi
+motd_install()
+{
+  echo "installing motd.sh"
+  motd_configure_os
+  motd_update_motd
+  motd_prompt_weather
+  motd_prompt_stock
+  motd_prompt_inspiration
 }
 
 motd_install
