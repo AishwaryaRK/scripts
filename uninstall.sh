5ca46142a8823cc3a5f3b029df1e1d445a26cfd4
Nick Baugh <niftylettuce@gmail.com>
2015-06-03 01:30:33 -0400

    Release 0.0.1

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# http://nathangrigg.net/2012/07/schedule-jobs-using-launchd/
-launchctl unload ~/Library/LaunchAgents/sh.motd.generator.plist
+motd_uninstall()
+{
+  # http://nathangrigg.net/2012/07/schedule-jobs-using-launchd/
+  launchctl unload ~/Library/LaunchAgents/sh.motd.generator.plist
+  rm ~/Library/LaunchAgents/sh.motd.generator.plist
+}
 
-rm ~/Library/LaunchAgents/sh.motd.generator.plist
+motd_uninstall "$@"
