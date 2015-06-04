be53482d18d17350541800bd4a94ffea1947dd00
Chad Furman <chad@chadfurman.com>
2015-06-04 19:40:55 -0400

    crontab mods started

--- includes/linux.sh
+++ includes/linux.sh
@@ -2,5 +2,17 @@
 
 motd_setup_generator()
 {
-  echo "Linux!";
+  CRONJOB="0 * * * * /home/$USER/.motd.sh"
+  CRONTAB=`crontab -l`
+
+  if echo $CRONTAB | grep "no crontab"; then
+    crontab -i
+    CRONTAB=`crontab -l`
+  fi
+
+  # crontab -l lists the current crontab jobs,
+  # cat prints it
+  # echo prints the new command
+  # and crontab - adds all the printed stuff into the crontab file.
+  (echo $CRONTAB | { grep -v "$CRONJOB"; echo "$CRONJOB"; }) | crontab -
 }
