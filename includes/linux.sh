633586f35c2d5ef14c5878a29fa7cf5e4cf70703
Chad Furman <chad@chadfurman.com>
2015-06-04 20:20:01 -0400

    overriding sed commands for compatibility in ubuntu

--- includes/linux.sh
+++ includes/linux.sh
@@ -16,3 +16,23 @@ motd_setup_generator()
   # and crontab - adds all the printed stuff into the crontab file.
   (echo $CRONTAB | { grep -v "$CRONJOB"; echo "$CRONJOB"; }) | crontab -
 }
+
+motd_configure_weather_address()
+{
+  sed -i'.bak' -e "s/WEATHER=\"\"/WEATHER=\"$1\"/" ~/.motd.sh
+}
+
+motd_configure_weather_degrees()
+{
+  sed -i'.bak' -e "s/DEGREES=\"\"/DEGREES=\"$1\"/" ~/.motd.sh
+}
+
+motd_configure_stocks()
+{
+  sed -i'.bak' -e "s/STOCKS=\"\"/STOCKS=\"$1\"/" ~/.motd.sh
+}
+
+motd_configure_quotes()
+{
+  sed -i'.bak' -e "s/QUOTES=\"\"/QUOTES=\"$1\"/" ~/.motd.sh
+}
